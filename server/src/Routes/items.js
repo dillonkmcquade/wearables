0cc9d628c0b6298e940988ed0a4564755bedecde
Dillon McQuade <dillonkmcquade@gmail.com>
2023-11-01 18:34:58 -0400

    remove unused code

--- server/src/Routes/items.js
+++ server/src/Routes/items.js
@@ -1,13 +1,9 @@
 const { Router } = require("express");
 const { getAllItems } = require("../handlers/getAllItems");
 const { getItemsByName } = require("../handlers/getItemsByName");
-const { getAllItemNames } = require("../handlers/getAllItemNames");
 
 const itemRouter = Router();
 
-itemRouter
-  .get("/", getAllItems)
-  .get("/:name", getItemsByName)
-  .get("/names", getAllItemNames);
+itemRouter.get("/", getAllItems).get("/:name", getItemsByName);
 
 module.exports = itemRouter;
